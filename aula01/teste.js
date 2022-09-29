d2d7220b41ff62a695641820429075b4fc684716
unknown <AlexandreGDEV@outlook.com>
2022-09-29 19:47:17 -0300

    add set interval e exibir porcentagem

--- aula01/teste.js
+++ aula01/teste.js
@@ -4,15 +4,23 @@ const os = require('os')
 
 //o mododulo os esta dentro da variável os, desta forma conseguimos acessar as propriedades e metodos deste módulo/objetp
 
-const totalMen = parseInt(os.totalmem()/1024 /1024 ) 
-const freeMen = parseInt(os.freemem()/1024 /1024 ) 
 
-const memoria = {
-    total: `${totalMen} MB`,
-    free: `${freeMen} MB`
-}
 
-console.clear()
-console.table(memoria)
 //console.log(`memoria livre: ${freeMen}`,`memoria livre: ${totalMen}`)
 //set interval
+
+setInterval(() => {
+    const totalMen = parseInt(os.totalmem()/1024 /1024 ) 
+    const freeMen = parseInt(os.freemem()/1024 /1024 ) 
+    const percent = parseInt((freeMen / totalMen) * 100)
+
+    const memoria = {
+        total: `${totalMen} MB`,
+        free: `${freeMen} MB`,
+        percent: `${percent} %` 
+    }
+    
+    
+    console.clear()
+    console.table(memoria)
+},1000)
